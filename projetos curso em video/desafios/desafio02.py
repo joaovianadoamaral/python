c2e84e3f18a29fea7bbc5c16250667274fbca5a9
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-06-13 18:41:29 -0300

    Initial commit

---
+++ projetos curso em video/desafios/desafio02.py
@@ -0,0 +1,8 @@
+print('===== desafio 02 =====')
+
+dia=input('dia= ')
+mes=input('mes= ')
+ano=input('ano= ')
+
+print('Você nasceu no dia',dia,'de '+mes+' de',ano,'. correto?')
+
---
+++ projetos curso em video/desafios/desafio04.py
@@ -0,0 +1,2 @@
+teste = input('digite algo na tela =')
+print('a classe primitiva do que voce digitou é \n')
