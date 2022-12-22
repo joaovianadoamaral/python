a59a3356d3609af63c9131d7ce408dc4fae4aa4c
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-12-22 15:00:59 -0300

    analisei e editeios codigos

--- projetos curso em video/desafios/desafio03.py
+++ projetos curso em video/desafios/desafio03.py
@@ -1,8 +1,8 @@
 print('===== desafio 03 =====')
-
+# Digite dois numeros e mostre a soma entre eles
 num1 = int(input('digite o primeiro numero: '))
 num2 = int(input('digite o segundo numero: '))
 
-soma = num1+num2
+soma = num1 + num2
 
-print('a soma é',soma)
+print('a soma é', soma)
