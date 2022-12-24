a880f2ea540986e87fc1277bbe1fd65beca85355
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-12-24 14:41:58 -0300

    a maioria das formas de manipular um texto

--- projetos curso em video/mundo_1/desafios/desafio21.py
+++ projetos curso em video/mundo_1/desafios/desafio21.py
@@ -1,3 +1,4 @@
-import pygame
-#como seria o programa caso o import do pygame fosse possivel
-
+from playsound import playsound
+#versões mais novas não possuem suporte ao pygame
+playsound('C:/Users/joaoj/OneDrive/Documentos/python/projetos curso em video/mundo_1/desafios/desafio21.mp3')
+#é preciso inverter as barras que por padrão vem invertidas
