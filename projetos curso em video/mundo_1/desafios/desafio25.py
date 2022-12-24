70e0855d7c6cac328428ca718d2738dd2010594e
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-12-24 17:27:10 -0300

    desafio25

--- projetos curso em video/mundo_1/desafios/desafio25.py
+++ projetos curso em video/mundo_1/desafios/desafio25.py
@@ -1,3 +1,5 @@
 print('{:=^25}'.format('Desafio 25'))
 #leia o nome de uma pessoa e verifique se ela possui 'Silva' no nome
-
+nome = str(input('Digite o seu nome completo -> '))
+aux = 'Silva' in nome
+print('O seu nome possui Silva? {}'.format(aux))
