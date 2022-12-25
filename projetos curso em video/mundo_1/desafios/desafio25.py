df346b364ca317176e24f6348d03d2bdd759e795
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-12-25 01:47:06 -0300

    caso silva esteja bagunçado

--- projetos curso em video/mundo_1/desafios/desafio25.py
+++ projetos curso em video/mundo_1/desafios/desafio25.py
@@ -1,5 +1,5 @@
 print('{:=^25}'.format('Desafio 25'))
 #leia o nome de uma pessoa e verifique se ela possui 'Silva' no nome
 nome = str(input('Digite o seu nome completo -> '))
-aux = 'Silva' in nome
+aux = 'SILVA' in nome.upper()
 print('O seu nome possui Silva? {}'.format(aux))
