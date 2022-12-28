1f1dbc885bf834774219892088d26f6b42687e32
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-12-28 11:11:46 -0300

    melhorando um pouco o programa

--- projetos curso em video/mundo_2/desafios/desafio37.py
+++ projetos curso em video/mundo_2/desafios/desafio37.py
@@ -12,14 +12,15 @@ escolha = int(input('{}Digite a opção de escolha:\n'
                     '-(2) para octal\n'
                     '-(3) para hexadecimal\n'
                     '->>> {}'.format('\033[33;40m', '\033[m')))
+#usar[2: significa que quero ignorar o indicativo de base como 0b, 0o e 0x
 if escolha == 1:
     binario = bin(num)
-    print('O número {} em binário é: {}'.format(num, binario))
+    print('O número {} em binário é: {}'.format(num, binario[2:]))
 elif escolha == 2:
     octal = oct(num)
-    print('O número {} em octal é: {}'.format(num, octal))
+    print('O número {} em octal é: {}'.format(num, octal[2:]))
 elif escolha == 3:
     hexadecimal = hex(num)
-    print('O número {} em hexadecimal é: {}'.format(num, hexadecimal))
+    print('O número {} em hexadecimal é: {}'.format(num, hexadecimal[2:]))
 else:
     print('{}Você digitou uma escolha invalida{}'.format('\033[31m', '\033[m'))
