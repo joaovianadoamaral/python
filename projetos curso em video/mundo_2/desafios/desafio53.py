3aff8523d4f20491796166a8d67dd7f48028a825
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-12-28 19:14:31 -0300

    desafio 53 incompleto

--- projetos curso em video/mundo_2/desafios/desafio53.py
+++ projetos curso em video/mundo_2/desafios/desafio53.py
@@ -1,11 +1,14 @@
 print('{:=^25}'.format('Desafio 53'))
 # Crie um programa que leia uma frase qualque e informe se ela é um palindromo desconsiderando os espaços
 frase = str(input('Digite uma frase qualquer-> ')).strip()
-frase = frase.split()
-rev = frase #necessário uma definição prévia para não ocorrer erro
+# eliminei os espaços da frase
+frase = ''.join(frase.split())
 tam = len(frase)
-#pega a frase inversa
+# pega a frase inversa
 aux = 0
-print(rev)
-print(frase)
-print(tam)
+rev = 'frase inversa'
+# verifica se o inverso é igual a frase digitada
+if frase == rev:
+    print('A sua frase é um palindromo')
+else:
+    print('A sua frase não é um palindromo')
