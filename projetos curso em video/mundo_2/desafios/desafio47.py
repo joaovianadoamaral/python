e6a82c4675cd704716d529b37734464e482e8c9a
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2022-12-28 16:49:00 -0300

    pares entre 1 e 50

---
+++ projetos curso em video/mundo_2/desafios/desafio47.py
@@ -0,0 +1,5 @@
+print('{:=^25}'.format('Desafio 47'))
+#faça um programa que mostre todos os números pares entre 1 e 50
+print('Veja todos os números pares entre 1 e 50')
+for par in range(2, 51, 2):
+    print(par)
