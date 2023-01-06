5249264b7a3c12878a8d63bad3a4ea1ef1eb6f74
João Vitor Viana <107434235+joaovianadoamaral@users.noreply.github.com>
2023-01-06 16:28:38 -0300

    Deixe seu código mais limpop

--- projetos-udemy/curiosidade.py
+++ projetos-udemy/curiosidade.py
@@ -1,21 +1,39 @@
 # use ... ou pass para não precisar escrever o código.
 
 #não é recomendado fazer conversões direto no input já que não se pode verificar o que o usuário digitou
-numero_1 = input('Digite um número: ')
-numero_2 = input('Digite outro número: ')
+# numero_1 = input('Digite um número: ')
+# numero_2 = input('Digite outro número: ')
 
-int_numero_1 = int(numero_1)
-int_numero_2 = int(numero_2)
+# int_numero_1 = int(numero_1)
+# int_numero_2 = int(numero_2)
 
-# quando se usa strings nomeadas, dela pra frente tem que nomear todas
-a=b=c=1
-string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
-formato = string.format(
-    nome1=a, nome2=b, nome3=c
-)
+# # quando se usa strings nomeadas, dela pra frente tem que nomear todas
+# a=b=c=1
+# string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+# formato = string.format(
+#     nome1=a, nome2=b, nome3=c
+# )
 
 # podemos usar a função format com indices sendo que para '{2}{1}{0}'.format(a,b,c)
 # será printado na tela as variaveis na ordem : c b a. já que a possui o indice 0, b o indice 1, e c o indice 2
 # quando é passado 3 argumentos no format(), não necessariamente se usará os 3.
 
 # escolha entre f-strings, método .format() e interpolação '%s, %f, %d'.
+
+# boas práticas de programação:
+'''
+no python não existe restrição de constantes em si.
+use letras maiusculas, por convenção para constantes.
+evite muitas condições no if.
+contagem de afastamento da margem. (quanto mais blocos dentro de blocos)
+'''
+
+# use \ para a continuação de código ex:
+
+if True and False \
+    and True: # a condição do laço if continua nessa linha
+    print(1)
+
+# no vs code vc consegue renomear todas as variaveis colocando o cursor nela e apertando f2
+
+# crlt + k + c - comentarios
